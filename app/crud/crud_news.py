142f0ea6cd00df22f463587c1962d908ce45bf48
Budi Ariyanto <fx.budi.ariyanto@gmail.com>
2024-04-25 23:54:19 +0700

    feat(redis): get data from redis when redis data exists, get data from db when redis data doesn't exists

--- app/crud/crud_news.py
+++ app/crud/crud_news.py
@@ -1,16 +1,36 @@
 from sqlalchemy.orm import Session
 from app.db.models.news import News
+from app.redis.client import redis_conn
+import json
 
 def get_news_items(db: Session, source: str = None, skip: int = 0, limit: int = 10):
+    redis_key = f"news:{source}:{skip}:{limit}"
+    if redis_conn.exists(redis_key):
+        redis_result = redis_conn.get(redis_key)
+        news = json.loads(redis_result)
+        return news
+
     query = db.query(News)
     if source:
         query = query.filter(News.source == source)
     news_items = query.offset(skip).limit(limit).all()
-    return news_items
+    news = [item.to_dict() for item in news_items]
+    redis_conn.set(redis_key, json.dumps(news))
+    return news
 
 def get_total_news_count(db: Session, source: str = None):
+    redis_key = f"news:{source}:count"
+    if redis_conn.exists(redis_key):
+        total_count = redis_conn.get(redis_key)
+        return int(total_count)
+
     query = db.query(News)
     if source:
         query = query.filter(News.source == source)
     total_count = query.count()
+    redis_conn.set(redis_key, total_count)
     return total_count
+
+def delete_news(db: Session, start_date: str, end_date: str):
+    db.query(News).filter(News.date >= start_date, News.date <= end_date).delete()
+    db.commit()
--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from app.api.routes import news as news_router
 from app.api.routes import health_check as health_check_router
+from app.redis.client import redis_conn
 
 app = FastAPI()
 
 app.include_router(news_router.router, prefix="/api/v1")
 app.include_router(health_check_router.router, prefix="/api/v1")
+
+@app.on_event("shutdown")
+def shutdown_event():
+    redis_conn.close()
