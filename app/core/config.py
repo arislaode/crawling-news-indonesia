7b8059ba8a2ace96f7f26d281df5c872f45581c1
odezig <odezig@odezigs-MacBook-Pro.local>
2024-05-02 21:58:57 +0700

    add env source crawling

--- app/core/config.py
+++ app/core/config.py
@@ -3,6 +3,12 @@ from starlette.config import Config
 
 config = Config(".env")
 
+DETIK_URL = config("DETIK_URL", cast=str)
+PERADABAN_URL = config("PERADABAN_URL", cast=str)
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+}
+
 DB_HOST = config("DB_HOST", cast=str)
 DB_USER = config("DB_USER", cast=str)
 DB_PASSWORD = config("DB_PASSWORD", cast=str)
