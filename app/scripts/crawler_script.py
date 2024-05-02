1f3839cd3722f195b2be9c8370250b6b85c94245
odezig <odezig@odezigs-MacBook-Pro.local>
2024-05-02 21:56:28 +0700

    change main crawling and add source peradaban

--- app/scripts/crawler_script.py
+++ app/scripts/crawler_script.py
@@ -1,18 +1,40 @@
-
+import logging
 from app.db.session import SessionLocal
-from app.scripts.parsers.detik import fetch_html, parse_and_save_to_db, URL
+from app.scripts.parsers.detik import fetch_html_detik, parse_and_save_to_db_detik
+from app.scripts.parsers.peradaban import fetch_html_peradaban, parse_and_save_to_db_peradaban
 from app.redis.client import redis_conn
+from app.core.config import DETIK_URL, PERADABAN_URL, HEADERS
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def crawl_detik(db):
+    try:
+        html_content_detik = fetch_html_detik(DETIK_URL, HEADERS)
+        if html_content_detik:
+            parse_and_save_to_db_detik(html_content_detik, DETIK_URL, db)
+    except Exception as e:
+        logging.error(f"An error occurred during the Detik crawl: {e}")
+
+def crawl_peradaban(db):
+    try:
+        html_content_peradaban = fetch_html_peradaban(PERADABAN_URL, HEADERS)
+        if html_content_peradaban:
+            parse_and_save_to_db_peradaban(html_content_peradaban, PERADABAN_URL, db)
+    except Exception as e:
+        logging.error(f"An error occurred during the Peradaban crawl: {e}")
 
 def run_crawler():
-    db = SessionLocal()
     try:
-        html_content = fetch_html(URL)
-        if html_content:
-            parse_and_save_to_db(html_content, db)
+        with SessionLocal() as db:
+            crawl_detik(db)
+            crawl_peradaban(db)
             redis_conn.flushdb()
+    except Exception as e:
+        logging.error(f"An error occurred in the run_crawler function: {e}")
     finally:
-        db.close()
         redis_conn.close()
+        logging.info("Redis connection closed")
 
 if __name__ == "__main__":
     run_crawler()
