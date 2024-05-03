1aa84923979f7cd0028a5ddb4ae3df7f57db46c2
odezig <odezig@odezigs-MacBook-Pro.local>
2024-05-03 22:35:03 +0700

    update response success

--- app/core/handle_response.py
+++ app/core/handle_response.py
@@ -8,13 +8,17 @@ def json_success_response(data: dict, status_code: int = status.HTTP_200_OK):
 def json_error_response(message: str, status_code: int = status.HTTP_400_BAD_REQUEST):
     return JSONResponse(status_code=status_code, content={"error": message})
 
-def detailed_json_response(status_code: int = status.HTTP_200_OK, message: str = "", data: list = None, total_items: int = 0, total_pages: int = 0, current_page: int = 0):
+def detailed_json_response(status_code: int = status.HTTP_200_OK, message: str = "", data: list = None, total_items: int = 0, total_pages: int = 0, current_page: int = 0, next_page: str = None, prev_page: str = None):
     content = {
         "status": "success" if status_code == status.HTTP_200_OK else "error",
         "message": message,
         "data": data,
-        "total_items": total_items,
-        "total_pages": total_pages,
-        "current_page": current_page
+        "meta": {
+            "total_items": total_items,
+            "total_pages": total_pages,
+            "current_page": current_page,
+            "next_page": next_page,
+            "prev_page": prev_page
+        }
     }
     return JSONResponse(status_code=status_code, content=content)
