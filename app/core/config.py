48806beb2e62c27c8521909709905245d8b4a473
odezig <odezig@odezigs-MacBook-Pro.local>
2024-05-03 19:17:54 +0700

    add config env kumparan

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,7 @@ DETIK_URL = config("DETIK_URL", cast=str)
 PERADABAN_URL = config("PERADABAN_URL", cast=str)
 TEMPO_URL = config("TEMPO_URL", cast=str)
 KOMPAS_URL=config("KOMPAS_URL", cast=str)
+KUMPARAN_URL=config("KUMPARAN_URL", cast=str)
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 }
