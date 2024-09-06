2378fcab3b28e1629c45f1193dd84991c4a6d9cd
Budi Ariyanto <fx.budi.ariyanto@gmail.com>
2024-09-06 13:44:58 +0700

    fix: news order

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.api.routes import news as news_router
 from app.api.routes import health_check as health_check_router
 from app.redis.client import redis_conn
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Application started")
+    yield
+    redis_conn.close()
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(news_router.router, prefix="/api/v1")
 app.include_router(health_check_router.router, prefix="/api/v1")
-
-@app.on_event("shutdown")
-def shutdown_event():
-    redis_conn.close()
